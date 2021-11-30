5b7092ea0aa9b4330edb86f8b96e8dd89b0bb932
oswenson-uwm <fscfedffee@gmail.com>
2021-11-30 13:36:45 -0600

    Added models.py

---
+++ GroupNullProject/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
