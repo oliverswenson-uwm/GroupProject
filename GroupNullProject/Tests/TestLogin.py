0ae5167b03ca89534e267b5b4716679fcffcb32c
oswenson-uwm <fscfedffee@gmail.com>
2021-11-30 15:30:32 -0600

    updated gitignore

--- GroupNullProject/Tests/TestLogin.py
+++ GroupNullProject/Tests/TestLogin.py
@@ -1,9 +1,14 @@
 from django.test import TestCase, Client
-
 from GroupNullProject.models import *
 
 class SuccessfulLogin(TestCase):
-    pass
+
+    client = None
+    def SetUp(self):
+        pass
+
+    def goodLogins(self):
+        pass
 
 class FailedLogin(TestCase):
 
@@ -12,34 +17,31 @@ class FailedLogin(TestCase):
     def setUp(self):
         self.client = Client()
         self.userList = {
-            "userone": "passone",
-            "usertwo": "passtwo",
-            "userthree": "passthree"
+            "userone": "passone", #user one
+            "usertwo": "passtwo", #user two
+            "userthree": "passthree" #user three
         }
-
         for i in self.userList.keys():
-
-            temp = MyUser(name=i, password = i)
+            temp = MyUser(name=i, password = userList.get(i))
             print(temp)
-            #temp.save()#save in database
+            temp.save() #save in database
 
+def test_passwordNotMatch(self):
+    resp = self.client.post("/", {"name": "userone", "password": "passtwo"}, follow=True)
+    self.assertEqual(resp.context["message"], "bad password", "no failed login. user:userone, pass:passtwo")
+    #TODO potentially change error message
 
-def test_badPasswordProvided(self):
-    resp = self.client.post("/", {"name": "one", "password": "three"}, follow=True)
-    self.assertEqual(resp.context["message"], "bad password", "no failed login password, user:one, pass:three")
-
-
-def test_OtherUserPassword(self):
-    resp = self.client.post("/", {"name": "one", "password": "two"}, follow=True)
-    self.assertEqual(resp.context["message"], "bad password",
-                     "no failed login password, user:one, pass:two, two is valid for another user")
-
+def test_userNotExist(self):
+    resp = self.client.post("/", {"name": "userdoesnotexist", "password": "passone"}, follow=True)
+    self.assertEqual(resp.context["message"], "user does not exist", "no failed login. user:userdoesnotexist, pass:passone")
 
 def test_noPasswordProvided(self):
     resp = self.client.post("/", {"name": "two", "password": ""}, follow=True)
     self.assertEqual(resp.context["message"], "bad password", "no failed login password, user:one, pass: NULL")
 
-
 def test_noUserProvided(self):
     resp = self.client.post("/", {"name": "", "password": "two"}, follow=True)
     self.assertEqual(resp.context["message"], "bad password", "no failed login password, user:NULL, pass: two")
+
+def test_twoBlankFields(self):
+    pass
