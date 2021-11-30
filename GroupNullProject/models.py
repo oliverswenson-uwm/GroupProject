df2fb1e959030f096ef930160239337d686643cf
Minwoo Lee <lee732@uwm.edu>
2021-11-30 13:50:57 -0600

    Changed models and add some stuff at testCourse.py

--- GroupNullProject/models.py
+++ GroupNullProject/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=80)
-    numCredits = models.IntegerField()
+    numCredits = models.IntegerFiel()
 
 class User(models.Model):
     name = models.CharField(max_length=20)
