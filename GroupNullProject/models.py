fe1db6c151e593656651355fc181e2b2d40a11bb
oswenson-uwm <fscfedffee@gmail.com>
2021-11-30 13:39:13 -0600

    updated models.py

--- GroupNullProject/models.py
+++ GroupNullProject/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Account(models.Model):
+    name = models.CharField(max_length=80)
+    numCredits = models.IntegerField()
 
 class User(models.Model):
     name = models.CharField(max_length=20)
