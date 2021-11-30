9e9c70d4431faeeabaf6767e3f2bb8cd1e8f6ebf
oswenson-uwm <fscfedffee@gmail.com>
2021-11-30 13:32:35 -0600

    Added test account create file

---
+++ GroupNullProject/Tests/testAccountCreate.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+#from myapp.models import (USER)
+
+def SetUp():
+    pass
+
+Class TestCreateValid(TestCase):
+    pass
+
+Class TestCreateInvalid(TestCase):
+    pass
+
+Class TestCreateDuplicate(TestCase):
+    pass
