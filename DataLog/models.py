6da73d0c6067b8667d15ed7c9162870b57e8ee16
oswenson-uwm <fscfedffee@gmail.com>
2021-12-14 14:10:56 -0600

    updated view assignments

--- DataLog/models.py
+++ DataLog/models.py
@@ -204,7 +204,8 @@ class Admin(Staff, models.Model):
 
         lab = Lab(name=name, section=section)
         lab.save()
-        labtocourse = LabToCourse(lab = lab, course = courseExist)
+        for e in courseExist:#will only run once just needed to pull value out of queryset
+            labtocourse = LabToCourse(lab = lab, course = e)
         labtocourse.save()
         return lab
 
@@ -315,13 +316,19 @@ class Professor(Staff, models.Model):
             for k in coursetolabobj:
                 labs.append(LabToCourse.getLab(self, k))
 
+
             for j in labs:  # iterate through the lab sections
-                str = i.course
-                str += " : "
-                str += j.lab
-                str += " : "
-                str += (TAToLab.getTA(j.lab))#in those labs, get each TA assigned to them
-                assignments.append(str)
+                labtotaobj = TAToLab.objects.filter(lab = j)
+                tas = []
+                for t in labtotaobj:
+                    tas.append(TAToLab.getTa(t))
+
+                    stri = str(i)#course
+                    stri += " : "
+                    stri += str(j)#lab
+                    stri += " : "
+                    stri += (t)#in those labs, get each TA assigned to them
+                    assignments.append(str)
                 #hoping to return a list like
                 # {[CS361 : Lab08 : Taiyu], [CS361 : Lab07 : Hossein], [CS 351 : Lab02 : Jimmy],}
                 #then we could display this list as a table on webpage.
