2d960b2edfdc6c5f53be093473030bb0974031a2
14144 <kravtso5@uwm.edu>
2021-12-17 12:28:41 -0600

    Changed assignUser to work with string prompts for username, course name and course section

--- GroupNullProject/UnitTests/TestAssignUser.py
+++ GroupNullProject/UnitTests/TestAssignUser.py
@@ -7,62 +7,78 @@ class TestAssignProf(TestCase):
 
     def setUp(self):
         self.admin = Admin(name="TestAdminone", email="adminonetest@gmail.com", username="adminonetestuser",
-                      password="adminpassone", phoneNum=9529529952, mailAddress="123 AdminTest Way")
+                           password="adminpassone", phoneNum=9529529952, mailAddress="123 AdminTest Way")
         self.prof1 = self.admin.createProf(fullName="TestprofOne", email="proftesting1@gmail.com",
-                                 username="testprofoneuser",
-                                 password="profpassone", phNumber=1231231233, mailAdrs="1 Professor St.")
+                                           username="testprofoneuser",
+                                           password="profpassone", phNumber=1231231233, mailAdrs="1 Professor St.")
         self.course1 = self.admin.createCourse(nm="MATH240", sec="001", cre="3", pre="None", des="matrices")
 
     def test_default(self):
-        assigned = self.admin.assignProf(self.prof1, course=self.course1)
-        self.assertEqual(assigned.__str__(), "Professor TestprofOne is assigned to course MATH240-001")
-        ProfessorToCourse.objects.get(id=assigned.id).delete()
+        self.assigned = self.admin.assignProf(self.prof1.username, self.course1.name, self.course1.section)
+        self.assertEqual(self.assigned.__str__(), "Professor TestprofOne is assigned to course MATH240-1")
 
     def test_noProfessor(self):
-        assigned = self.admin.assignProf(prof=None, course=self.course1)
-        self.assertEqual(assigned, None)
+        self.assigned = self.admin.assignProf(None, self.course1.name, self.course1.section)
+        self.assertEqual(self.assigned, None)
 
     def test_noCourse(self):
-        assigned = self.admin.assignProf(prof=self.prof1, course=None)
-        self.assertEqual(assigned, None)
+        self.assigned = self.admin.assignProf(self.prof1.username, None, None)
+        self.assertEqual(self.assigned, None)
+
+    def test_noCourseSection(self):
+        self.assigned = self.admin.assignProf(self.prof1.username, self.course1.name, None)
+        self.assertEqual(self.assigned, None)
+
+
+    def test_multipleProf(self):
+        self.temp = self.admin.assignProf(self.prof1.username, self.course1.name, self.course1.section)
+        self.prof2 = self.admin.createProf(fullName="TestprofTwo", email="proftesting2@gmail.com",
+                                           username="testproftwouser",
+                                           password="profpasstwo", phNumber=3213213211, mailAdrs="2 Professor St.")
+        self.assigned = self.admin.assignProf(self.prof2.username, self.course1.name, self.course1.section)
+        self.assertEqual(self.assigned, None)
 
     def test_duplicateProf(self):
-        temp = self.admin.assignProf(prof=self.prof1, course=self.course1)
-        prof2 = self.admin.createProf(fullName="TestprofTwo", email="proftesting2@gmail.com",
-                                      username="testproftwouser",
-                                      password="profpasstwo", phNumber=3213213211, mailAdrs="2 Professor St.")
-        assigned = self.admin.assignProf(prof=prof2, course=self.course1)
-        self.assertEqual(assigned, None)
-        ProfessorToCourse.objects.get(id=temp.id).delete()
+        self.temp = self.admin.assignProf(self.prof1.username, self.course1.name, self.course1.section)
+        self.assigned = self.admin.assignProf(self.prof1.username, self.course1.name, self.course1.section)
+        self.assertEqual(self.assigned, None)
 
 
 # noinspection SpellCheckingInspection
 class TestAssignTA(TestCase):
     def setUp(self):
         self.admin = Admin(name="TestAdminone", email="adminonetest@gmail.com", username="adminonetestuser",
-                  password="adminpassone", phoneNum=9529529952, mailAddress="123 AdminTest Way")
+                           password="adminpassone", phoneNum=9529529952, mailAddress="123 AdminTest Way")
         self.ta1 = self.admin.createTA(fullName="TestTAone", email="taOnGmail1@gmail.com", username="testTAone",
-                         password="testpassoneTA",
-                         phNumber=3334441111, mailAdrs="2 TeachingAssistant Circle")
+                                       password="testpassoneTA",
+                                       phNumber=3334441111, mailAdrs="2 TeachingAssistant Circle")
         self.course1 = self.admin.createCourse(nm="MATH240", sec="001", cre="3", pre="None", des="matrices")
 
     def test_default(self):
-        assigned = self.admin.assignTA(ta=self.ta1, course=self.course1)
-        self.assertEqual(assigned.__str__(), "TA TestTAone is assigned to course MATH240-001")
-        TAToCourse.objects.get(id=assigned.id).delete()
+        self.assigned = self.admin.assignTAToCourse(self.ta1.username, self.course1.name, self.course1.section)
+        self.assertEqual(self.assigned.__str__(), "TA TestTAone is assigned to course MATH240-1")
 
     def test_noTA(self):
-        assigned = self.admin.assignTA(ta=None, course=self.course1)
-        self.assertEqual(assigned, None)
+        self.assigned = self.admin.assignTAToCourse(None, self.course1.name, self.course1.section)
+        self.assertEqual(self.assigned, None)
 
-    def test_noLab(self):
-        assigned = self.admin.assignTA(ta=self.ta1, course=None)
-        self.assertEqual(assigned, None)
+    def test_noCourse(self):
+        self.assigned = self.admin.assignTAToCourse(self.ta1.username, None, None)
+        self.assertEqual(self.assigned, None)
+
+    def test_noCourseSection(self):
+        self.assigned = self.admin.assignTAToCourse(self.ta1.username, self.course1.name, None)
+        self.assertEqual(self.assigned, None)
+
+    def test_multipleTA(self):
+        self.temp = self.admin.assignTAToCourse(self.ta1.username, self.course1.name, self.course1.section)
+        self.ta2 = self.admin.createTA(fullName="TestTAtwo", email="taTwoGmail1@gmail.com", username="testTAtwo",
+                                       password="testpasstwoTA", phNumber=3334441111,
+                                       mailAdrs="3 TeachingAssistant Circle")
+        self.assigned = self.admin.assignTAToCourse(self.ta2.username, self.course1.name, self.course1.section)
+        self.assertEqual(self.assigned, None)
 
     def test_duplicateTA(self):
-        temp = self.admin.assignTA(ta=self.ta1, course=self.course1)
-        ta2 = self.admin.createTA(fullName="TestTAtwo", email="taTwoGmail1@gmail.com", username="testTAtwo",
-                                  password="testpasstwoTA", phNumber=3334441111, mailAdrs="3 TeachingAssistant Circle")
-        assigned = self.admin.assignTA(ta=ta2, course=self.course1)
-        self.assertEqual(assigned, None)
-        TAToCourse.objects.get(id=temp.id).delete()
+        self.temp = self.admin.assignTAToCourse(self.ta1.username, self.course1.name, self.course1.section)
+        self.assigned = self.admin.assignTAToCourse(self.ta1.username, self.course1.name, self.course1.section)
+        self.assertEqual(self.assigned, None)
