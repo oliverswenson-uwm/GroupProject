786338c6f9ccfb2d3baae80bd4c5dc5eef9ec234
oswenson-uwm <fscfedffee@gmail.com>
2021-12-19 14:46:20 -0600

    finished acceptance tests for archive account

---
+++ GroupNullProject/AcceptanceTests/testArchiveAcc.py
@@ -0,0 +1,78 @@
+from django.test import TestCase
+from DataLog.models import Staff, Admin, Professor, TA
+from django.test import Client
+
+
+class TestArchiveValid(TestCase):
+
+    def setUp(self):
+        client = None
+        client = Client()
+
+    def testSuccessfulArchiveOne(self):
+        Admin.createAdmin(self, fullName="ArchiveAdmin", email="Archivethisadmin@gmail.com", username="adminarchive",
+                          password="adminarchiveme", phNumber=1234567889, mailAdrs="7 admin Lane")
+
+        resp = self.client.post("/archiveacc/", {"email": "Archivethisadmin@gmail.com", "username": "adminarchive", "password": "adminarchiveme",
+                                                 "accType": "Admin"
+                                                 }, follow=True)
+        self.assertEqual("Success! Archived account.", resp.context["msg"], "Account didn't archive but it shouldve")
+
+
+    def testSuccessfulArchiveTwo(self):
+        Admin.createAdmin(self, fullName="ArchiveAdminTwo", email="Archivethisadminagain@gmail.com", username="archivemetoo",
+                          password="archivepass", phNumber=1112221211, mailAdrs="7 archive Lane")
+
+        resp = self.client.post("/archiveacc/", {"email": "Archivethisadminagain@gmail.com", "username": "archivemetoo", "password": "archivepass",
+                                                 "accType": "Admin"
+                                                 }, follow=True)
+        self.assertEqual("Success! Archived account.", resp.context["msg"], "Account didn't archive but it shouldve")
+
+    #if only enter username of account should still find and archive the account
+    #(for ease for admin if they forget street address or phone number)
+    def testSuccessfulWithMissingInfo(self):
+        Admin.createAdmin(self, fullName="ArchiveAdminTwo", email="Archivethisadminagain@gmail.com",
+                          username="archivemetoo",
+                          password="archivepass", phNumber=1112221211, mailAdrs="7 archive Lane")
+
+        resp = self.client.post("/archiveacc/", {"email": "", "username": "archivemetoo",
+                                                 "password": "",
+                                                 "accType": "Admin"
+                                                 }, follow=True)
+
+        self.assertEqual("Success! Archived account.", resp.context["msg"], "Account didn't archive but it shouldve")
+
+
+#INVALID TESTS
+class TestArchiveInvalid(TestCase):
+
+    def setUp(self):
+        client = None
+        client = Client()
+
+    def testMissingFields(self):
+        Admin.createAdmin(self, fullName="ArchiveAdminTwo", email="Archivethisadminagain@gmail.com",
+                          username="archivemetoo",
+                          password="archivepass", phNumber=1112221211, mailAdrs="7 archive Lane")
+
+        resp = self.client.post("/archiveacc/", {"email": "", "username": "",
+                                                 "password": "",
+                                                 "accType": ""
+                                                 }, follow=True)
+
+        self.assertEqual("Failed to archive account. Double check the information entered", resp.context["msg"], "Recieved success message when should have recieved error")
+
+    def testArchiveTwice(self):
+        Admin.createAdmin(self, fullName="ArchiveAdmin", email="Archivethisadmin@gmail.com", username="adminarchive",
+                          password="adminarchiveme", phNumber=1234567889, mailAdrs="7 admin Lane")
+
+        self.client.post("/archiveacc/", {"email": "Archivethisadmin@gmail.com", "username": "adminarchive",
+                                                 "password": "adminarchiveme",
+                                                 "accType": "Admin"
+                                                 }, follow=True)
+        resp = self.client.post("/archiveacc/", {"email": "Archivethisadmin@gmail.com", "username": "adminarchive",
+                                                 "password": "adminarchiveme",
+                                                 "accType": "Admin"
+                                                 }, follow=True)
+
+        self.assertEqual("Failed to archive account. Double check the information entered", resp.context["msg"], "Account archived twice")
